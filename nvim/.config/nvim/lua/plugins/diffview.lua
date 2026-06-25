3cc70afb761cb3f2caf6969fd0f9f85a0aa5719b
jp-belanger <8102483+jp-belanger@users.noreply.github.com>
2026-06-25 14:00:18 +0000

    nvim: simplify diffview config

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,9 +1,4 @@
 return {
   "sindrets/diffview.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("diffview").setup {
-      -- Your custom configuration goes here (optional)
-    }
-  end,
 }

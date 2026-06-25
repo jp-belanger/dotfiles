d10161619b2456ec7b0cfc7b15075f7cd39bd9b8
jp-belanger <8102483+jp-belanger@users.noreply.github.com>
2026-06-25 14:25:30 +0000

    nvim: simplify plugin setup specs

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,12 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup {
-        options = {
-          theme = "auto",
-          component_separators = "",
-          section_separators = "",
-        },
-      }
-    end,
+    opts = {
+      options = {
+        theme = "auto",
+        component_separators = "",
+        section_separators = "",
+      },
+    },
   },
 }

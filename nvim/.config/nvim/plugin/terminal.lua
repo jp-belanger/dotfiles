90c059ec9123cacf5c6da23a86b1e6f4003d9bca
jp-belanger <8102483+jp-belanger@users.noreply.github.com>
2026-06-25 14:15:34 +0000

    nvim: clean up terminal autocmds

--- nvim/.config/nvim/plugin/terminal.lua
+++ nvim/.config/nvim/plugin/terminal.lua
@@ -1,10 +1,19 @@
-local api = vim.api
+local group = vim.api.nvim_create_augroup("Terminal", { clear = true })
 
--- Insert more when entering a terminal buffer
-local InsertOnTerminalGrp = api.nvim_create_augroup("InsertOnTerminal", { clear = true })
-api.nvim_create_autocmd("BufEnter", {
-  command = "if &buftype == 'terminal' | startinsert | endif",
-  group = InsertOnTerminalGrp,
+-- Enter insert mode when entering a terminal buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = group,
+  callback = function()
+    if vim.bo.buftype == "terminal" then
+      vim.cmd.startinsert()
+    end
+  end,
 })
 
-api.nvim_create_autocmd("TermOpen", { command = "setlocal nonumber norelativenumber" })
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = group,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
